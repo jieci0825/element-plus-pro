98d8d02bab9c5071918c2b25ac7383a2ef69a62c
coderjc <coderjc@qq.com>
2025-11-01 00:25:14 +0800

    feat: 添加 menu 组件结构

---
+++ packages/components/menu/index.ts
@@ -0,0 +1,6 @@
+import { SFCWithInstall, withInstall } from '@coderjc/element-plus-pro-utils'
+import Menu from './src/menu.vue'
+
+export const ProMenu: SFCWithInstall<typeof Menu> = withInstall(Menu)
+
+export default ProMenu
