4d68cae233f8e920ac93a597691fe84e7f049466
coderjc <coderjc@qq.com>
2025-11-19 01:55:03 +0800

    remove: 移除菜单组件封装

--- packages/components/menu/index.ts
+++
@@ -1,6 +0,0 @@
-import { SFCWithInstall, withInstall } from '@coderjc/element-plus-pro-utils'
-import Menu from './src/menu.vue'
-
-export const ProMenu: SFCWithInstall<typeof Menu> = withInstall(Menu)
-
-export default ProMenu
