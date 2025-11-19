1026e33c47e94cd2a5023a8128fe7ef540c523ae
coderjc <coderjc@qq.com>
2025-11-19 13:58:23 +0800

    fix: make ProFormProps type partial to allow for optional properties

--- packages/components/form/src/form.ts
+++ packages/components/form/src/form.ts
@@ -62,7 +62,7 @@ export const proFormProps = {
     // 其他属性则会直接透传给 el-form
 } as const
 
-export type ProFormProps = ExtractPropTypes<typeof proFormProps> &
+export type ProFormProps = Partial<ExtractPropTypes<typeof proFormProps>> &
     Omit<FormInstance['$props'], 'modelValue'>
 
 export const proFormEmits = {
